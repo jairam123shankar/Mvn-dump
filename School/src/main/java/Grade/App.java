2102afe7bac20822e034706b8ff7371c536b55cf
jairam <jairanshankarashetty@gmail.com>
2023-01-28 09:50:08 +0000

    first commit

---
+++ School/src/main/java/Grade/App.java
@@ -0,0 +1,13 @@
+package Grade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World tody is my birthday!" );
+    }
+}
